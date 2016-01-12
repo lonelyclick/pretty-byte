04ee0783f589c3d4e49665432708de08ba10f665
杨柳岸kkk <lonelyclick@163.com>
2016-01-12 16:11:15 +0800

    change arc

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 'use strict';
-var numberIsNan = require('number-is-nan');
 
 module.exports = function (num) {
-	if (typeof num !== 'number' || numberIsNan(num)) {
-		throw new TypeError('Expected a number');
-	}
-
-	var exponent;
-	var unit;
-	var neg = num < 0;
-	var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-	if (neg) {
-		num = -num;
-	}
-
-	if (num < 1) {
-		return (neg ? '-' : '') + num + ' B';
-	}
-
-	exponent = Math.min(Math.floor(Math.log(num) / Math.log(1024)), units.length - 1);
-	num = Number((num / Math.pow(1024, exponent)).toFixed(2));
-	unit = units[exponent];
-
-	return (neg ? '-' : '') + num + ' ' + unit;
+	if (isNaN(parseFloat(num)) || !isFinite(num)) return '-';
+	var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+	number = Math.floor(Math.log(num) / Math.log(1024));
+	return (num / Math.pow(1024, Math.floor(number))).toFixed(2) +  ' ' + units[number];
 };
